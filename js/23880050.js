fcb5d4ce524e490fbf50ec9ebe99552558e1173e
JONS102 <106539800+JONS102@users.noreply.github.com>
2025-04-26 07:49:54 +0700

    Test Login

--- js/23880050.js
+++ js/23880050.js
@@ -1,4 +1,5 @@
 const API = "https://web1-api.vercel.app/api";
+const AUTHENTICATE_API = "https://web1-api.vercel.app/users";
 // cách lấy data ta từ API về
 async function loadData(request, templateId, viewId) {
     const response = await fetch(`${API}/${request}`);
@@ -11,4 +12,24 @@ async function loadData(request, templateId, viewId) {
     var context = { data: data };
     var view = document.getElementById(viewId);
     view.innerHTML = template(context);
+}
+
+async function getAuthenticateToken(username, password) {
+    let response = await fetch(`${AUTHENTICATE_API}/authenticate`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            username,
+            password
+        })
+    }); // tới linh users/authenticate để lấy phần xác nhận gởi mail
+    let result = response.json();
+    if (response.status == 200) {
+        return result.token; // trả về token, laays token để xác thực người dùng
+    } else {
+        throw new Error("Authentication failed");
+    }
 }
